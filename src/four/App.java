4f886d9533b562d7442a875e63162e97763c8943
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-04-18 16:59:05 +0300

    Five

--- src/four/App.java
+++ src/four/App.java
@@ -1,4 +1,17 @@
 package four;
 
 public class App {
+
+    static <T> String S(words<T> tool, T[] vals, T t){
+                                                        //used by String interface (words)
+       return "" + tool.function(vals,t);               //to access int and string G method in Metod.java
+
+    }
+
+
+
+    public static void main(String... args){
+
+
+    }
 }
