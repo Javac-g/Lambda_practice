021a4f4e320e5f8eac56d374f11683371ad06415
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-04-20 19:08:04 +0300

    second

--- src/five/App.java
+++ src/five/App.java
@@ -1,6 +1,23 @@
 package five;
 
+
+
 public class App {
+
+    static <T> Integer I(I_Tool<T> tool, T[] vals, T t){
+
+        return tool.function(vals,t);
+
+    }
+    static <V> String S(S_Tool<V> tool, V[] vals,V v){
+
+        return "" + tool.function(vals,v);
+
+    }
+    static <T,V> T A(A_Tool<T,V> tool, V[] vals, V v){
+        return tool.function(vals,v);
+
+    }
 
     public static void main(String...args){
 
