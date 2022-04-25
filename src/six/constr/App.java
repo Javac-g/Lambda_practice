bdca84112433ed71ebee1540db791e70da2a8fd1
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-04-25 23:09:15 +0300

    11

--- src/six/constr/App.java
+++ src/six/constr/App.java
@@ -3,7 +3,13 @@ package six.constr;
 public class App {
     public static void main(String...args){
 
+        Tool tool = box::new;
+        box a = tool.function(1,2,3);
 
+
+        System.out.println("w: " + a.getW());
+        System.out.println("H: " + a.getH());
+        System.out.println("L: " + a.getL());
 
 
     }
