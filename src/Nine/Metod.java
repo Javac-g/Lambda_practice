ad638284644f20f6207204ba6893367d54818709
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-04-30 23:14:52 +0300

    seven

---
+++ src/Nine/Metod.java
@@ -0,0 +1,19 @@
+package Nine;
+
+public class Metod {
+
+    static <T> String words (T[] values, T t){
+        int count = 0;
+
+        for (int i = 0; i <= values.length-1; i++){
+
+            if(values[i] == t)count++;
+
+        }
+        return "" + count;
+
+    }
+
+
+
+}
