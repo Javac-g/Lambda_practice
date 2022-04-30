f2c0a9e26c472073ac0d495cdbc2dd7d6255786c
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-04-30 23:33:45 +0300

    twelve

--- src/Nine/App.java
+++ src/Nine/App.java
@@ -1,6 +1,11 @@
 package Nine;
 
 public class App {
+    static <T> String S_runner(Stool<T> tool, T[] values, T t){
+
+        return tool.function(values,t);
+
+    }
     public static void main(String ...args){
 
 
