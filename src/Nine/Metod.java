da117d243d8b26aa3cfc4aa8432f6e2ad0f9b207
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-04-30 23:16:25 +0300

    eight

--- src/Nine/Metod.java
+++ src/Nine/Metod.java
@@ -14,6 +14,19 @@ public class Metod {
 
     }
 
+    static <T> Integer digits(T[] values, T t){
+
+        int count = 0;
+
+        for (int i = 0; i <= values.length-1; i++){
+
+            if(values[i] == t)count++;
+
+        }
+        return  count;
+
+    }
+
 
 
 }
