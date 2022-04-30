43cd855d36e1ccc910a8f79597971561a07cbfcf
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-04-30 23:54:42 +0300

    2

---
+++ src/Nine/Box_two.java
@@ -0,0 +1,26 @@
+package Nine;
+
+public class Box_two <T>{
+
+    T w,h,l;
+
+    Box_two(T w, T h,T l){
+
+        this.w = w;
+        this.h = h;
+        this.l = l;
+
+    }
+
+    public T getW() {
+        return w;
+    }
+
+    public T getH() {
+        return h;
+    }
+
+    public T getL() {
+        return l;
+    }
+}
