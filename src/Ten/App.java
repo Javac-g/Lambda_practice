18fed89fd0f930137553f89a1866bf9567dde7ed
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-05-03 23:42:08 +0300

    12

--- src/Ten/App.java
+++ src/Ten/App.java
@@ -1,6 +1,12 @@
 package Ten;
 
 public class App {
+
+    static <T> String s_run(Stool<T> tool,T[] values, T t){
+
+        return  tool.function(values,t);
+
+    }
     public static void main(String...args){
 
 
