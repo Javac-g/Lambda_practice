8673936b3a509e47b59bfdf938713e8f5d76b73e
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-05-04 22:37:26 +0300

    18

--- src/L1/App.java
+++ src/L1/App.java
@@ -25,7 +25,15 @@ public class App {
         Integer[] nums = {1,2,3,4,5,3,2,1,34,5,3,2,1,1,11,1,1,1,1};
         String[] str ={"one" , "one" , "one" ,"one"};
 
+        System.out.println(S_run(Metod::words,str,"one"));
+        System.out.println(S_run(Metod::words,nums,1));
+        System.out.println(I_run(Metod::digits,str,"one"));
+        System.out.println(I_run(Metod::digits,nums,1));
 
+        System.out.println("" + A_run(Metod::words,str,"one"));
+        System.out.println("" + A_run(Metod::words,nums,1));
+        System.out.println("" + A_run(Metod::digits,str,"one"));
+        System.out.println("" + A_run(Metod::digits,nums,1));
 
 
 
