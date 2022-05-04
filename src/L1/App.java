40387190272df8c5201a6de47aeb4ad021725c02
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-05-04 22:26:26 +0300

    16

--- src/L1/App.java
+++ src/L1/App.java
@@ -2,7 +2,16 @@ package L1;
 
 public class App {
 
+
+    static <T> String S_run(Stool<T> tool, T[] values, T t){
+
+        return  tool.function(values,t);
+
+    }
     public static void main(String...args){
+
+        Integer[] nums = {1,2,3,4,5,3,2,1,34,5,3,2,1,1,11,1,1,1,1};
+        String[] str ={"one" , "one" , "one" ,"one"};
 
 
 
