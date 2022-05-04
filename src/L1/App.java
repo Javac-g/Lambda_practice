7e5fe5922c7a5743293d50f6c4f909770283b32b
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-05-04 22:31:29 +0300

    17

--- src/L1/App.java
+++ src/L1/App.java
@@ -8,6 +8,18 @@ public class App {
         return  tool.function(values,t);
 
     }
+
+    static <T> Integer I_run(Itool<T> tool , T[] values,T t){
+
+        return tool.function(values,t);
+
+    }
+
+    static <V,T> V A_run(Atool<V,T> tool, T[] values, T t){
+
+        return tool.function(values,t);
+
+    }
     public static void main(String...args){
 
         Integer[] nums = {1,2,3,4,5,3,2,1,34,5,3,2,1,1,11,1,1,1,1};
