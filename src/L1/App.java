d5998f08258c750ddb927549d99b8477d2e3b108
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-05-04 22:17:58 +0300

    12

---
+++ src/L1/App.java
@@ -0,0 +1,17 @@
+package L1;
+
+public class App {
+
+    public static void main(String...args){
+
+
+
+
+
+
+
+
+
+    }
+
+}
---
+++ src/L1/Metod.java
@@ -0,0 +1,34 @@
+package L1;
+
+public class Metod {
+
+    static <T> String words(T[] values, T t){
+
+        int count = 0;
+
+        for (int i = 0; i <= values.length-1; i++){
+
+            if(values[i] == t)count++;
+
+        }
+
+        return "" + count;
+
+    }
+
+    static <T> Integer digits(T[] values, T t){
+
+        int count = 0;
+
+        for (int i = 0; i <= values.length-1; i++){
+
+            if(values[i] == t)count++;
+
+        }
+
+
+        return  count;
+
+    }
+
+}
