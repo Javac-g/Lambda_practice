dd10786735d87132b2b07458336edc3ad2e753f7
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-05-05 23:11:12 +0300

    13

--- src/L2/Constr/App.java
+++ src/L2/Constr/App.java
@@ -1,9 +1,16 @@
 package L2.Constr;
 
 public class App {
+
+
+
     public static void main(String...args){
 
+        A tool = Box::new;
 
+        Box one = tool.function(12,13,14);
+
+        //Easy but not usefull
 
 
 
