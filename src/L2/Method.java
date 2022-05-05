98b75f0d2dd4ee545131b866aef9298215e867f4
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-05-05 22:50:46 +0300

    six

--- src/L2/Method.java
+++ src/L2/Method.java
@@ -17,7 +17,19 @@ public class Method {
 
     }
 
+    static <T> Integer digit(T[] values, T t){
 
+        int count = 0;
+
+        for (int i = 0; i <= values.length-1; i++){
+
+            if(values[i] == t)count++;
+
+        }
+
+        return count;
+
+    }
 
 
 
