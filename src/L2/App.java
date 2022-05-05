1e35b63aadcee01b4f09b2993771f233520bbb85
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-05-05 22:55:56 +0300

    seven

--- src/L2/App.java
+++ src/L2/App.java
@@ -2,6 +2,16 @@ package L2;
 
 public class App {
 
+    static Integer[] N = {1,2,3,4,51,1,1,1,1,1,1,1,1,1,1,2,2,22,2};
+    static String[] W = {"one" , "two", "three" , "one" , "one"};
+
+    static <T> String S_run(Stool<T> tool,T[] values, T t){
+
+        return tool.function(values,t);
+
+    }
+
+    static <T> Integer I_run(Itool<T> tool,T[] values)
     public static void main(String...args){
 
 
