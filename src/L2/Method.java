ea62cf1fb5560ab64326507a035c6649e1693fc7
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-05-05 22:48:29 +0300

    five

---
+++ src/L2/Method.java
@@ -0,0 +1,30 @@
+package L2;
+
+public class Method {
+
+
+    static <T> String word(T[] values,T t){
+
+        int count = 0;
+
+        for (int i = 0; i <= values.length-1; i++){
+
+            if(values[i] == t)count++;
+
+        }
+
+        return "Value: " + count;
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+}
