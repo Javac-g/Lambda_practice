77da9b6fd59926df3f74d467e201ad73f6794c32
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-05-10 22:41:26 +0300

    two

--- src/L3/Method.java
+++ src/L3/Method.java
@@ -1,4 +1,21 @@
 package L3;
 
 public class Method {
+
+
+    static <T> String words(T[] values, T t){
+
+        int count = 0;
+
+        for (int i = 0; i <= values.length -1; i++){
+
+            if(values[i] == t)count++;
+
+
+        }
+        return "" + count;
+
+    }
+
+
 }
