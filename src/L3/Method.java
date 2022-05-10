e0c05cdd661d438a4c2965af8aef212989c26be0
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-05-10 22:43:11 +0300

    three

--- src/L3/Method.java
+++ src/L3/Method.java
@@ -17,5 +17,19 @@ public class Method {
 
     }
 
+    static <T> Integer digits(T[] values, T t){
+
+        int count = 0;
+
+        for (int i = 0; i <= values.length -1; i++){
+
+            if(values[i] == t)count++;
+
+
+        }
+        return  count;
+
+    }
+
 
 }
