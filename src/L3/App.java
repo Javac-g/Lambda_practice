4be1327f7f69d9f940cbadcb5566a98f27dcd213
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-05-10 23:07:14 +0300

    ten

--- src/L3/App.java
+++ src/L3/App.java
@@ -17,7 +17,23 @@ public class App {
        return tool.function(values,t);
 
     }
+
+
     public static void main(String...args){
+        Integer[] nums ={1,2,3,4,5,5,5,5,5,5,5,5,55,5,5};
+        String[] str = {"one","one","one","one","one","one","one","one","one",};
+
+        System.out.println("Values: " + S_run(Method::words,nums,5));
+        System.out.println("Values: " + S_run(Method::words,str,"one"));
+        System.out.println("Values: " + I_run(Method::digits,str,"one"));
+        System.out.println("Values: " + I_run(Method::digits,nums,5));
+
+        System.out.println("Values: " + All(Method::words,nums,5));
+        System.out.println("Values: " + All(Method::words,str,"one"));
+        System.out.println("Values: " + All(Method::digits,str,"one"));
+        System.out.println("Values: " + All(Method::digits,nums,5));
+
+
 
 
 
