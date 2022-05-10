6840d0c18473ba7a94463fde6f97fd94a2c4f3b8
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-05-10 22:52:01 +0300

    eight

--- src/L3/App.java
+++ src/L3/App.java
@@ -7,6 +7,11 @@ public class App {
         return tool.function(values,t);
 
     }
+    static <T> Integer I_run(Itool<T> tool , T[] values, T t){
+
+        return tool.function(values,t);
+
+    }
     public static void main(String...args){
 
 
