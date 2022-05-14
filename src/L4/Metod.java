55d30cdc2b7e620616410db7461208fc446c59b1
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-05-14 22:42:48 +0300

    6

---
+++ src/L4/Metod.java
@@ -0,0 +1,19 @@
+package L4;
+
+public class Metod {
+
+
+    public <T> String digits(T[] values, T t){
+
+        int count = 0;
+
+        for (int i = 0; i <=values.length-1; i++){
+
+            if (values[i] == t)count++;
+
+        }
+        return "" +  count;
+    }
+
+    pu
+}
