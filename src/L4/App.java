5a0d39252fca2fa852cd7b4b98b599211ef62e3f
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-05-14 22:55:15 +0300

    11

--- src/L4/App.java
+++ src/L4/App.java
@@ -1,6 +1,11 @@
 package L4;
 
 public class App {
+    static <T> String A(Stool<T> tool,T[] values, T t){
+
+        return tool.function(values,t);
+
+    }
     public static void main(String...args){
 
 
