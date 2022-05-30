30666e82478c31d04106326f36c575d593d99d7f
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-05-30 23:41:19 +0300

    8

--- src/L5/App.java
+++ src/L5/App.java
@@ -6,6 +6,16 @@ public class App {
         return tool.function(values,t);
 
     }
+    static <T> Integer I_Runner(Itool<T> tool,T[] values, T t){
+
+        return tool.function(values,t);
+
+    }
+    static <V,T> V A_Runner(Atool<V,T> tool, T[] values,T t){
+
+        return tool.function(values,t);
+
+    }
     public static void main(String...args){
 
 
