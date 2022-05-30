7a85ac07d7b042f383f2004c9ae63988a28dae50
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-05-30 23:38:46 +0300

    7

--- src/L5/App.java
+++ src/L5/App.java
@@ -1,4 +1,18 @@
 package L5;
 
 public class App {
+    static <T> String S_Runner(Stool<T> tool,T[] values,T t){
+
+        return tool.function(values,t);
+
+    }
+    public static void main(String...args){
+
+
+
+
+
+
+
+    }
 }
