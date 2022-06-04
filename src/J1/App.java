8d163404b56cf85559570bcb1c4a483affe2af6f
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-06-04 22:56:46 +0300

    Cube

--- src/J1/App.java
+++ src/J1/App.java
@@ -1,14 +1,18 @@
 package J1;
 
 public class App {
-    static <V,T> V creator()
+    static <V,T> V creator(Gen<V,T> tool,T w, T h, T l){
+
+        return tool.function(w,h,l);
+
+    }
     public static void main(String...args){
 
         Box_I one = Box::new;
 
         Box box = one.function(12,13,14);
 
-
+        Gen<>
 
     }
 }
---
+++ src/J1/Cube.java
@@ -0,0 +1,37 @@
+package J1;
+
+public class Cube <T>{
+    T w,h,l;
+
+    Cube(T w, T h , T l){
+
+        this.w = w;
+        this.h = h;
+        this.l = l;
+
+    }
+
+    public T getW() {
+        return w;
+    }
+
+    public void setW(T w) {
+        this.w = w;
+    }
+
+    public T getH() {
+        return h;
+    }
+
+    public void setH(T h) {
+        this.h = h;
+    }
+
+    public T getL() {
+        return l;
+    }
+
+    public void setL(T l) {
+        this.l = l;
+    }
+}
