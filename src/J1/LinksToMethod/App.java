383180d5b6dcaa3e0ba362665b8d12353ab6a22c
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-06-04 23:35:21 +0300

    String generic  method

--- src/J1/LinksToMethod/App.java
+++ src/J1/LinksToMethod/App.java
@@ -1,6 +1,11 @@
 package J1.LinksToMethod;
 
 public class App {
+    static <S> String S_Runner(Stool<S> tool, S[] values, S s){
+
+        return tool.function(values,s);
+
+    }
     public static void main(String...args){
 
 
