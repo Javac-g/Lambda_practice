6472ec77968c6687c07ebbf9181550be73e6039f
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-06-04 22:51:55 +0300

    Generic  interface

--- src/J1/App.java
+++ src/J1/App.java
@@ -1,9 +1,12 @@
 package J1;
 
 public class App {
+    static <V,T> V creator()
     public static void main(String...args){
 
+        Box_I one = Box::new;
 
+        Box box = one.function(12,13,14);
 
 
 
---
+++ src/J1/Gen.java
@@ -0,0 +1,7 @@
+package J1;
+
+public interface Gen < V , T > {
+
+    V function(T w, T h, T l);
+
+}
