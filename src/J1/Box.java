88c7abcae36fdfd517181e24cc42617b3de1ac5a
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-06-04 22:38:38 +0300

    Box

---
+++ src/J1/Box.java
@@ -0,0 +1,35 @@
+package J1;
+
+public class Box {
+    Integer w,h,l;
+
+    public Box(Integer w, Integer h, Integer l) {
+        this.w = w;
+        this.h = h;
+        this.l = l;
+    }
+
+    public Integer getW() {
+        return w;
+    }
+
+    public void setW(Integer w) {
+        this.w = w;
+    }
+
+    public Integer getH() {
+        return h;
+    }
+
+    public void setH(Integer h) {
+        this.h = h;
+    }
+
+    public Integer getL() {
+        return l;
+    }
+
+    public void setL(Integer l) {
+        this.l = l;
+    }
+}
