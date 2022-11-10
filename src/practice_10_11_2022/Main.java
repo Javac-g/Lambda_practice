3f3e8faada7b2721a5ab99950238a6d9dcb35751
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-11-10 16:21:04 +0200

    simple lambda

---
+++ src/practice_10_11_2022/Main.java
@@ -0,0 +1,8 @@
+package practice_10_11_2022;
+
+public class Main {
+    public static void main(String...args){
+        Tool x = str -> str.toUpperCase();
+        System.out.println(x.function("hello"));
+    }
+}
---
+++ src/practice_10_11_2022/Tool.java
@@ -0,0 +1,5 @@
+package practice_10_11_2022;
+
+public interface Tool {
+    String function(String str);
+}
