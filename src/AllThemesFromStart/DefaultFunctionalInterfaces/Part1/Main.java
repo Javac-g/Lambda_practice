fb00c8405e36376323b3a2f1971d0b3a124c0e0e
Maxym-Denysov <adamsmatthew70@yahoo.com>
2023-04-06 17:06:52 +0300

    UnaryOperator

--- src/AllThemesFromStart/DefaultFunctionalInterfaces/Part1/Main.java
+++ src/AllThemesFromStart/DefaultFunctionalInterfaces/Part1/Main.java
@@ -2,19 +2,25 @@ package AllThemesFromStart.DefaultFunctionalInterfaces.Part1;
 
 import java.util.function.BinaryOperator;
 import java.util.function.Function;
+import java.util.function.UnaryOperator;
 
 public class Main {
     public static <T,V> V process(Function<T,V> function,T num){
         return function.apply(num);
     }
     public static void main(String...args){
-        Function<Integer,Double> arg = (Integer num) -> num.doubleValue();
+        Function<Integer,Double> function = (Integer num) -> num.doubleValue();
+        UnaryOperator<Double> unaryOperator = (Double x) -> {
+            x = x + 2.0;
+            return x ;
+        };
         BinaryOperator<Integer> binaryOperator = (Integer x,Integer y)->{
             x += y;
             return x + y;
         };
-        System.out.println(process(arg,22));
-        System.out.println(arg.apply(9));
+        System.out.println(process(function,22));
+        System.out.println(function.apply(9));
+        System.out.println(unaryOperator.apply(99.9));
         System.out.println(binaryOperator.apply(12,13));
     }
 }
